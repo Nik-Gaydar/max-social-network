28f87a46fc2878be645c9a3647d69dbe4abbaca9
Nikita Gaydar <NikGaydar@yandex.ru>
2026-03-06 09:40:16 +0700

    refactor: updated the imports in the file

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import './index.css'
-import App from './App.tsx'
 import { store } from './app/store/store.ts'
+import App from './app/App.tsx'
+import './app/styles'
 
 const root = document.getElementById('root')
 if (!root) throw new Error('Missing #root element')
